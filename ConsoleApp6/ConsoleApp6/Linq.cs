7487f568bd7ade5d20b4d93e88f8113f9b863cb8
moeinheydaridev <moeinheydaridev@gmail.com>
2024-08-04 14:51:21 +0330

    Add files via upload

---
+++ ConsoleApp6/ConsoleApp6/Linq.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    public class Linq
+    {
+        public string Name;
+        public double AVG;
+        public double DN;
+
+        public Linq(string name, double avg, double dn)
+        {
+            Name = name;
+            AVG = avg;
+            DN = dn;
+
+        }
+    }
+}
---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,42 @@
+﻿using ConsoleApp6;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+{
+    List<Linq> linq = new List<Linq>();
+    linq.Add(new Linq("Amir", 90, 200));
+    linq.Add(new Linq("AHMAD", 80, 400));
+    linq.Add(new Linq("REZA", 95, 400));
+    linq.Add(new Linq("MOEIN", 100, 2000));
+
+    var result = linq.OrderByDescending( x => x.AVG ).ToList();
+    var re = linq.Where( x => x.AVG > 94 ).ToList();
+    var doo = linq.Select( x => x.Name ).ToList();
+    var didi = linq.Sum( x => x.AVG ).ToString();
+    
+    Console.WriteLine("these are soretd by biggest avg");
+
+    foreach (var item in result)
+    {
+        Console.WriteLine($"{"  "+ item.Name + " " + item.AVG + " " + item.DN}");
+        Console.WriteLine("*******************");
+    }
+    Console.WriteLine("these are sorted by biggest avg since 94");
+
+    foreach (var item in re)
+    {
+        Console.WriteLine($"{"  " + item.Name + " " + item.AVG + " " + item.DN}");
+        Console.WriteLine("*******************");
+    }
+    Console.WriteLine("these are sorted by just name ");
+
+    foreach (var item in doo)
+    {
+        Console.WriteLine(item);
+        Console.WriteLine("*******************");
+    }
+    Console.WriteLine("sum of the avg");
+    Console.WriteLine(didi);
+    Console.WriteLine("**************************");
+
+}
